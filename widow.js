4d49897e5d2b9a08e0c326731f6d3744fb224cb7
Murilo Bastos <murilo.silva@justdigital.com.br>
2016-03-08 16:05:17 -0300

    Removing jQuery dependency

--- widow.js
+++ widow.js
@@ -1,36 +1,80 @@
-$(document).ready(function () {
-	var $parents = $('.text');
-	var $texts = $('.text p');
-	$texts.each(function (i, text) {
-		var $parent = $($parents[i]);
-		var $text = $(text);
-		// Checa se o texto está passando o limite da div
-		if ($text.prop('scrollWidth') > $text.outerWidth()) {
-			console.log($text.prop('scrollWidth'), $text.outerWidth());
-			// Transforma o texto em um array de palavras
-			var words = $text.text().split(' ');
-			// Determina um limitador baseado na metade da quantidade de palavras.
-			// Deve ser aprimorado pra aceitar textos com mais de uma linha
-			var count = words.length - ~~ (words.length / 2); // ~~ é o mesmo que parseInt(), porem mais performatico
+var Widow = {
+	defaultClass: "text",
+	defaultElement: "p",
+	parents: [],
+	texts: [],
+	addNoWrapToText: true,
 
-			// Cria duas variaveis para receber cada linha
-			// Deve ser mudado para array quando for adicionar suporte a mais de duas linhas
-			var newWords1 = '';
-			var newWords2 = '';
+	getParents: function () {
+		var parentElements = document.getElementsByClassName(this.defaultClass);
+		var parentsArr = [];
 
-			// Preenche a primeira linha com a metade da array de palavras
-			for (var i = 0; i < count; i++) {
-				newWords1 += words[i] + ' ';
-			};
-
-			// Preenche a segunda linha com o restante de palavras
-			for (var i = count; i < words.length; i++) {
-				newWords2 += words[i] + ' ';
-			};
-
-			// Deleta o texto atual e substitui por duas linhas
-			$parent.html('');
-			$parent.append('<p>' + newWords1 + '</p><p>' + newWords2 + '</p>')
+		for (p in parentElements) {
+			var el = parentElements[p];
+			if (typeof el === 'object') {
+				var children = el.querySelectorAll(this.defaultElement);
+				var parentObj = {
+					el: el,
+					children: children
+				};
+				parentsArr.push(parentObj);
+			}
 		}
-	});
-});
+
+		return parentsArr;
+	},
+
+	parseParent: function (parent) {
+		var parentEl = parent.el;
+		for (var c in parent.children) {
+			var childEl = parent.children[c];
+			
+			// Checa se o texto está passando o limite da div
+			if (childEl.scrollWidth > childEl.offsetWidth) {
+
+				// Transforma o texto em um array de palavras
+				var words = childEl.innerText.split(' ');
+
+				// Determina um limitador baseado na metade da quantidade de palavras.
+				// Deve ser aprimorado pra aceitar textos com mais de uma linha
+				var count = words.length - ~~ (words.length / 2); // ~~ é o mesmo que parseInt(), porem mais performatico
+
+				// Cria duas variaveis para receber cada linha
+				// Deve ser mudado para array quando for adicionar suporte a mais de duas linhas
+				var newWords1 = '';
+				var newWords2 = '';
+
+				// Preenche a primeira linha com a metade da array de palavras
+				for (var i = 0; i < count; i++) {
+					newWords1 += words[i] + ' ';
+				};
+
+				// Preenche a segunda linha com o restante de palavras
+				for (var i = count; i < words.length; i++) {
+					newWords2 += words[i] + ' ';
+				};
+				// Deleta o texto atual e substitui por duas linhas
+				parent.el.innerHTML = '<p>' + newWords1 + '</p><p>' + newWords2 + '</p>';
+			}
+		}
+	},
+
+	parseParents: function (parents) {
+		for (var p in parents) {
+			var parent = parents[p];
+			this.parseParent(parent);
+		}
+	},
+
+	init: function () {
+		// Pega o array de parents
+		var parentList = this.getParents();
+
+		// Inicia o parse de cada parent
+		this.parseParents(parentList);
+	}
+};
+
+(function() {
+	Widow.init();
+})();
