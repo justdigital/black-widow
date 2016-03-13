d5600b2afe649b88d295ba00229015409bef56ff
Daniel Galvão <galvaodev@gmail.com>
2016-03-13 00:35:43 -0300

    Improving performance of iterations

--- widow.js
+++ widow.js
@@ -7,9 +7,10 @@ var Widow = {
 
 	getParents: function () {
 		var parentElements = document.getElementsByClassName(this.defaultClass);
+		var parentElmLength = parentElements.length;
 		var parentsArr = [];
 
-		for (var p = 0; p < parentElements.length; p++) {
+		for (var p = 0; p < parentElmLength; p++) {
 			var el = parentElements[p];
 			var children = el.querySelectorAll(this.defaultElement);
 			var parentObj = {
@@ -26,30 +27,32 @@ var Widow = {
 		var parentEl = parent.el;
 		for (var c in parent.children) {
 			var childEl = parent.children[c];
-			
+
 			// Checa se o texto está passando o limite da div
 			if (childEl.scrollWidth > childEl.offsetWidth) {
 
 				// Transforma o texto em um array de palavras
 				var words = childEl.innerText.split(' ');
+				var wordsLength = words.length;
 
 				// Determina um limitador baseado na metade da quantidade de palavras.
 				// Deve ser aprimorado pra aceitar textos com mais de uma linha
-				var count = words.length - ~~ (words.length / 2); // ~~ é o mesmo que parseInt(), porem mais performatico
+				var count = wordsLength - ~~ (wordsLength / 2); // ~~ é o mesmo que parseInt(), porem mais performatico
 
 				// Cria duas variaveis para receber cada linha
 				// Deve ser mudado para array quando for adicionar suporte a mais de duas linhas
 				var newWords1 = '';
 				var newWords2 = '';
 
-				// Preenche a primeira linha com a metade da array de palavras
-				for (var i = 0; i < count; i++) {
-					newWords1 += words[i] + ' ';
-				};
 
-				// Preenche a segunda linha com o restante de palavras
-				for (var i = count; i < words.length; i++) {
-					newWords2 += words[i] + ' ';
+				for (var i = 0; i < wordsLength; i++) {
+					if (i < count) {
+						// Preenche a primeira linha com a metade da array de palavras
+						newWords1 += words[i] + ' ';
+					} else {
+						// Preenche a segunda linha com o restante de palavras
+						newWords2 += words[i] + ' ';
+					}
 				};
 				// Deleta o texto atual e substitui por duas linhas
 				parent.el.innerHTML = '<p>' + newWords1 + '</p><p>' + newWords2 + '</p>';
